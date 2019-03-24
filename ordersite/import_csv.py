fa12296e1519038bc9556c4162cd351e28774aa9
JohnLiu <joinmytripweb@gmail.com>
2019-03-24 14:08:42 +0800

    Try -- Setup django before run python script

--- ordersite/import_csv.py
+++ ordersite/import_csv.py
@@ -1,8 +1,11 @@
 import csv
 import os
 from datetime import datetime
+import django
 path =  os.getcwd()
 os.chdir(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ordersite.settings")
+django.setup()
 from orderapp.models import Order, OrderItem
 
 with open('order.csv') as csvfile:
